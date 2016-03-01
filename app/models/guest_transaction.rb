053a55108954a01ca991aa4d6697514724d4c867
Jon Stokes <jon@jonstokes.com>
2016-03-01 15:06:10 -0600

    Validations

--- app/models/guest_transaction.rb
+++ app/models/guest_transaction.rb
@@ -1,24 +1,22 @@
 class GuestTransaction
   include Virtus.model
+  include ActiveModel::Model
 
-  class Guest
-    include Virtus.model
-    attribute :device_address
-    attribute :access_point_address
-    attribute :url
-  end
+  attribute :device_address
+  attribute :access_point_address
+  attribute :url
+  attribute :first_name
+  attribute :last_name
+  attribute :cc_number
+  attribute :city
+  attribute :state
+  attribute :zip
+  attribute :security_code
 
-  class Transaction
-    include Virtus.model
-    attribute :first_name
-    attribute :last_name
-    attribute :cc_number
-    attribute :city
-    attribute :state
-    attribute :zip
-    attribute :security_code
-  end
-
-  attribute :guest, GuestTransaction::Guest
-  attribute :transaction, GuestTransaction::Transaction
+  validates :device_address,            format: { with: /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/ }
+  validates :access_point_address,      format: { with: /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$/ }
+  validates :url,                       format: { with: URI.regexp }
+  validates :first_name, :last_name,    presence: true
+  validates :cc_number, :security_code, presence: true
+  validates :city, :state, :zip,        presence: true
 end
