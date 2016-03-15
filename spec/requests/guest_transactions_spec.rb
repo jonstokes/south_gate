87d5919dd6807d94a3ddca2227a2549159bb7527
Jon Stokes <jon@jonstokes.com>
2016-03-15 15:54:34 -0500

    Failing request spec

--- spec/requests/guest_transactions_spec.rb
+++ spec/requests/guest_transactions_spec.rb
@@ -1,9 +1,9 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe "GuestTransactions", type: :request do
   describe "GET /guest_transactions" do
     it "works! (now write some real specs)" do
-      get guest_transactions_path
+      get new_guest_transaction_path
       expect(response).to have_http_status(200)
     end
   end
